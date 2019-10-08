3174fda51add1f282eff7daa648f43ac6c254844
donker <peter@bring2mind.net>
2019-10-08 10:14:37 +0200

    Move globbing methods to proper cake extensions and its own class

--- Utilities.cs
+++ Utilities.cs
@@ -1,6 +1,4 @@
-﻿using Cake.Common.IO;
-using Cake.Core;
-using Cake.Core.IO;
+﻿using Cake.Core;
 using System;
 
 namespace Dnn.CakeUtils
@@ -17,86 +15,6 @@ namespace Dnn.CakeUtils
             ai.SetProperty("AssemblyCompany", sln.dnn.owner.organization);
             ai.SetProperty("AssemblyCopyright", string.Format("Copyright {0} by {1}", System.DateTime.Now.Year, sln.dnn.owner.organization));
             ai.Write();
-        }
-
-        public static FilePathCollection GetFilesByPatterns(ICakeContext context, string[] patterns)
-        {
-            FilePathCollection res = context.GetFiles(patterns[0]);
-            for (var i = 1; i < patterns.Length - 1; i++)
-            {
-                res += context.GetFiles(patterns[i]);
-            }
-            return res;
-        }
-
-        public static FilePathCollection GetFilesByPatterns(ICakeContext context, string[] patterns, Func<IDirectory, bool> predicate)
-        {
-            var settings = new GlobberSettings();
-            settings.Predicate = predicate;
-            settings.IsCaseSensitive = false;
-            FilePathCollection res = context.GetFiles(patterns[0], settings);
-            for (var i = 1; i < patterns.Length - 1; i++)
-            {
-                res += context.GetFiles(patterns[i], settings);
-            }
-            return res;
-        }
-
-        public static FilePathCollection GetFilesByPatterns(ICakeContext context, string root, string[] patterns)
-        {
-            root = root.EnsureEndsWith("/");
-            FilePathCollection res = context.GetFiles(root + patterns[0]);
-            for (var i = 1; i < patterns.Length - 1; i++)
-            {
-                res += context.GetFiles(root + patterns[i]);
-            }
-            return res;
-        }
-
-        public static FilePathCollection GetFilesByPatterns(ICakeContext context, string root, string[] patterns, Func<IDirectory, bool> predicate)
-        {
-            root = root.EnsureEndsWith("/");
-            var settings = new GlobberSettings();
-            settings.Predicate = predicate;
-            settings.IsCaseSensitive = false;
-            FilePathCollection res = context.GetFiles(root + patterns[0], settings);
-            for (var i = 1; i < patterns.Length - 1; i++)
-            {
-                res += context.GetFiles(root + patterns[i], settings);
-            }
-            return res;
-        }
-
-        public static Func<IFileSystemInfo, bool> ExcludeFunction(string[] excludeFilter)
-        {
-            return ExcludeFunction(excludeFilter, true);
-        }
-        public static Func<IFileSystemInfo, bool> ExcludeFunction(string[] excludeFilter, bool start)
-        {
-            return fileSystemInfo =>
-            {
-                var crt = new System.IO.DirectoryInfo(".");
-                var rel = fileSystemInfo.Path.FullPath.Substring(crt.FullName.Length + 1);
-                foreach (var ef in excludeFilter)
-                {
-                    //Console.WriteLine("{0} =? {1}", rel, ef);
-                    if (start)
-                    {
-                        if (rel.StartsWith(ef))
-                        {
-                            return false;
-                        }
-                    }
-                    else
-                    {
-                        if (rel.EndsWith(ef))
-                        {
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            };
         }
 
         public static string GetTextOrMdFile(string filePathWithoutExtension)
@@ -124,7 +42,7 @@ namespace Dnn.CakeUtils
 
         public static void CreateResourcesFile(ICakeContext context, string path, string packagePath, string packageName, string[] releaseFiles, string[] excludeFiles)
         {
-            var files = GetFilesByPatterns(context, path, releaseFiles, ExcludeFunction(excludeFiles));
+            var files = context.GetFilesByPatterns(path, releaseFiles, excludeFiles);
             if (files.Count > 0)
             {
                 var resZip = Compression.ZipToBytes(path, files);
